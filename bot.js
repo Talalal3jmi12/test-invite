6d5297e1ccb6e23b137d06cda433a77e10e3f62f
Talalal3jmi12 <47371530+Talalal3jmi12@users.noreply.github.com>
2019-02-14 14:19:25 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,43 @@
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("516965469672964099");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
+
+
+
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("516965470260428821");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("516965469672964099");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;         
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
+
+
+
+client.login(NTQ0OTgwMjM1NzM3NzU5Nzc0.D0Vfgw.c8lMsGSNmnUcjbV4zUjh9i8dcUg);
